53689ead22794e8e9219c75acdacd6c50ca2f1fe
Sam Burkhard <samryanburk@icloud.com>
2023-05-30 11:08:17 -0400

    Added first version of DebugMenu

---
+++ vomo_ios/vomo/Core/Views/DebugMenuView.swift
@@ -0,0 +1,58 @@
+//
+//  DebugMenuView.swift
+//  VoMo
+//
+//  Created by Sam Burkhard on 5/29/23.
+//
+
+import SwiftUI
+
+struct DebugMenuView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationView {
+            
+            List {
+                
+                Section {
+                    
+                    Button {
+                        // do it
+                    } label: {
+                        Text("Print scheduled notifications")
+                    }
+                    
+                } header: {
+                    Text("Notifications")
+                }
+            }
+            
+                .navigationTitle("Debug Menu")
+                .navigationBarTitleDisplayMode(.inline)
+            
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            self.dismiss()
+                        } label: {
+                            Text("Done")
+                        }
+
+                    }
+                }
+        }
+    }
+}
+
+struct DebugMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            
+        }
+        .sheet(isPresented: .constant(true)) {
+            DebugMenuView()
+        }
+    }
+}
