7ae18aa87eb66319a8a1cdb97ca803ad0175cb3d
Sam Burkhard <samryanburk@icloud.com>
2023-06-25 17:12:29 -0400

    Big async bugs

--- vomo_ios/vomo/Core/Views/RecordView/FeedbackRecordView.swift
+++ vomo_ios/vomo/Core/Views/RecordView/FeedbackRecordView.swift
@@ -9,36 +9,45 @@ import Foundation
 import SwiftUI
 
 class WaveformPointsDummyStream {
+        
+    private var continuation: AsyncStream<Float?>.Continuation?
     
-    static let shared = WaveformPointsDummyStream()
-    
-    private var continuation: AsyncStream<Int?>.Continuation?
-    
-    public func push(_ item: Int?) {
-        continuation?.yield(item)
+    public func cancel() {
+        continuation?.finish()
+//        stream = nil
     }
     
-    lazy var stream: AsyncStream<Int?> = {
-        AsyncStream(Int?.self, bufferingPolicy: .bufferingNewest(1)) { cont in
+     init() {
+        
+        let amplitude: Float = 0.20  // Amplitude of the sine wave
+        let frequency: Float = 10.0  // Frequency of the sine wave (in Hz)
+        var time: Float = 0.0
+
+        let _ = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+            let value = amplitude * sin(2 * Float.pi * frequency * time) + 0.5
+            time += 0.001
+            self.continuation?.yield(value)
+        }
+    }
+    
+    lazy var stream: AsyncStream<Float?> = {
+        AsyncStream(Float?.self, bufferingPolicy: .bufferingNewest(1)) { cont in
             continuation = cont
-                
+            
             cont.onTermination = { @Sendable status in
                 print("Task terminated with status: \(status)")
             }
         }
     }()
     
-    
 }
 
 class WaveformPointsManager: ObservableObject {
-    
-    static let shared = WaveformPointsManager(count: 130)
-        
+                    
     struct Points {
         
         let count: Int
-        private let buffer: UnsafeMutableBufferPointer<Int?>
+        private let buffer: UnsafeMutableBufferPointer<Float?>
         
         init(count: Int) {
             self.count = count
@@ -47,7 +56,7 @@ class WaveformPointsManager: ObservableObject {
             buffer.initialize(repeating: nil)
         }
         
-        func add(_ point: Int?) {
+        func add(_ point: Float?) {
             if buffer.baseAddress != nil {
                 // shift up to last item
                 for address in 0..<(count - 1) {
@@ -62,44 +71,70 @@ class WaveformPointsManager: ObservableObject {
             buffer.deallocate()
         }
         
-        subscript(index: Int) -> Int? {
+        subscript(index: Int) -> Float? {
             return buffer[index]
         }
         
     }
     
+    static let shared = WaveformPointsManager(count: 130)
+    
     @Published var points: Points
+    
+    public var consumer: Task<(), Never>?
+    public var stream: WaveformPointsDummyStream?
     
     public init(count: Int) {
         self.points = Points(count: count)
+    }
+    
+    func update(date: Date) {}
+    
+    public func observeStreams() {
         
-        // consume streams
-        Task {
-            for await point in WaveformPointsDummyStream.shared.stream {
-                points.add(point)
+        stream = WaveformPointsDummyStream()
+        
+        if stream != nil {
+            consumer = Task {
+                
+                do {
+                    for await point in self.stream!.stream {
+                        try Task.checkCancellation()
+                        self.points.add(point)
+                    }
+                } catch {
+                    print("Cancelled")
+                }
+                
             }
         }
     }
     
-    func update(date: Date) { }
+    public func cancelStreamObservation() {
+        consumer?.cancel()
+        stream = nil
+    }
     
     // memory-safety
     deinit {
         points.deallocate()
+        print("WaveformPointsManager Deinit")
     }
     
 }
 
 struct FeedbackWaveform: View {
     
-    @ObservedObject private var waveform: WaveformPointsManager = .shared
+    @StateObject var waveform: WaveformPointsManager = .shared
+    
+    public let size: CGSize
     
     // config visual elements here
     private let pointSpacing: CGFloat = 3.0
     private let pointSize: CGSize = .init(width: 4, height: 4)
     
     var body: some View {
-        HStack {
+        VStack {
             TimelineView(.animation) { timeline in
                                 
                 Canvas { context, size in
@@ -109,14 +144,24 @@ struct FeedbackWaveform: View {
                     // waveform
                     for index in stride(from: 0, to: waveform.points.count, by: 1) {
                         
-                        if let y = waveform.points[index] {
+                        if let point = waveform.points[index] {
                             let x = CGFloat(index) * pointSpacing
-                            let rect = CGRect(origin: CGPoint(x: x, y: CGFloat(y)), size: pointSize)
+//                            let x = size.width
+                            let y = (CGFloat(point) * size.height)
+                            let rect = CGRect(origin: CGPoint(x: x, y: y), size: pointSize)
                             let path = Circle().path(in: rect)
                             context.fill(path, with: .color(.MEDIUM_PURPLE))
                         }
                     }
                 }
+            }
+            
+            .onAppear {
+                waveform.observeStreams()
+            }
+            
+            .onDisappear {
+                waveform.cancelStreamObservation()
             }
         }
     }
@@ -126,22 +171,24 @@ struct FeedbackWaveform: View {
 struct FeedbackRecordView: View {
     
     // MARK: Delete later
-    @State private var dummyInput: Float = 770 / 2
+//    @State private var dummyInput: Float = 0.5
     
     var body: some View {
         
         VStack {
             
-            FeedbackWaveform()
+            GeometryReader { proxy in
+                FeedbackWaveform(size: proxy.size)
+            }
             
-            Slider(value: $dummyInput, in: 0...770)
-                .padding()
-                .onAppear(perform: {
-                    let _ = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
-                        WaveformPointsDummyStream.shared.push(Int(dummyInput))
-                    }
-                })
-                .tint(.MEDIUM_PURPLE)
+//            Slider(value: $dummyInput, in: 0...1)
+//                .padding()
+//                .onAppear(perform: {
+//                    let _ = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+//                        WaveformPointsDummyStream.shared.push(Float(dummyInput))
+//                    }
+//                })
+//                .tint(.MEDIUM_PURPLE)
         }
     }
 }
